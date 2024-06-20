628d59d778dafdf7d61c47fea8ff68f91159b6dd
saibharath-16 <108922762+saibharath-16@users.noreply.github.com>
2024-06-20 19:22:07 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,17 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Body from './Body'
 
-function App() {
-  const [count, setCount] = useState(0)
+// import ColorBox from "./components/ColorBox";
+import Counter from "./components/Counter";
+import MyFunc from "./components/MyFunc";
+import Navbar from "./components/Navbar";
+import StateMang from "./components/StateMang";
+import Theme from "./components/Theme";
+import Toggle from "./components/Toggle";
+import { Routes,Route } from "react-router-dom";
+import HomeBtn from "./components/HomeBtn";
+import Error from "./components/Error";
+import Profile from "./components/Profile";
+import Clock from "./components/Clock";
+import ImgSlide from "./components/ImgSlide";
 
-  return (
-    <>
-      <Body />
-    </>
+
+function App(){
+  return(
+    
+    <div className="holder">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomeBtn/>}/>
+        <Route path="/toggle" element={ <Toggle />} />
+        <Route path="/theme" element={ <Theme />}/>
+        <Route path="/myfunc" element={<MyFunc />}/>
+        <Route path="/statemang" element={<StateMang />}/>
+        <Route path="/counter" element={<Counter />}/>
+        <Route path="*" element={<Error />}/>
+        <Route path="/profile" element={Profile}/>
+       </Routes><br/>
+       <HomeBtn /><br/><br/>
+       <Clock />
+       <ImgSlide />
+    </div>
+   
   )
 }
 
-export default App
+export default App;
+
+
