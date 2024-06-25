331b2cab154113909f65fcd9a533bad5a6b8a621
saibharath-16 <108922762+saibharath-16@users.noreply.github.com>
2024-06-25 18:14:40 +0530

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import AppRouter from './AppRouter';
+import './App.css'
+import Body from './Body'
+import Timer from './Timer'
+import FetchingApi from './FetchingApi'
+import Component2 from './Component2'
+import { createContext } from 'react';
+import Component1 from './Component1'
 
-const App = () => {
+export const varName = createContext()
+
+function App() {
   return (
-    <div className="App">
-      <AppRouter />
-    </div>
-  );
+    <>
+      {/* <Body />
+      <Timer /> */}
+      {/* <FetchingApi /> */}
+      <varName.Provider value={"Ready to change color"}>
+        <Component2 />
+      </varName.Provider>
+
+    </>
+  )
 }
 
-export default App;
+export default App
